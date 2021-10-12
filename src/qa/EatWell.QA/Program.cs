83bd968996a2a86383e3f04f609d33447accf9a9
M-Hasbi <kaynakhasbi1@gmail.com>
2021-10-12 14:42:57 +0200

    create EatWell.QA solution

--- src/qa/EatWell.QA/Program.cs
+++ src/qa/EatWell.QA/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EatWell.QA
 {
@@ -6,7 +10,6 @@ namespace EatWell.QA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
