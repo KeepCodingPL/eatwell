2596546b9a4a259376726cdf739edc2c55421db2
Elif Aygun <elifdilaa@gmail.com>
2021-10-18 21:50:35 +0200

    New folder DTO for requests and responses

---
+++ src/api/EatWell.API/Persistence/EatWellContext.cs
@@ -0,0 +1,14 @@
+﻿using EatWell.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EatWell.API.Persistence
+{
+    public class EatWellContext : DbContext
+    {
+        public DbSet<ProductModel> Products { get; set; }
+    }
+}
