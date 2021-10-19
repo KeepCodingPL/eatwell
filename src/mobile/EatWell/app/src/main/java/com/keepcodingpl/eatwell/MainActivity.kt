4db59ac9e06a1409193144cde1e259105f9aef3c
Suleyman Oner <suleymanoner1999@gmail.com>
2021-10-19 23:55:33 +0200

    view-pager added

--- src/mobile/EatWell/app/src/main/java/com/keepcodingpl/eatwell/MainActivity.kt
+++ src/mobile/EatWell/app/src/main/java/com/keepcodingpl/eatwell/MainActivity.kt
@@ -2,10 +2,28 @@ package com.keepcodingpl.eatwell
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.keepcodingpl.eatwell.Adapter.ViewPagerAdapter
+import com.keepcodingpl.eatwell.FeedFragment.FeedFragment
+import com.keepcodingpl.eatwell.MyPostsFragment.MyPostsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createViewPager()
+
     }
+
+    private fun createViewPager() {
+        viewPagerAdapter.addFragment(FeedFragment(), "FEED")
+        viewPagerAdapter.addFragment(MyPostsFragment(), "MY POSTS")
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
 }
