112feac72d8a6fca753e4a40eac7558554ff7a71
ozturksalih <salihozturk020@gmail.com>
2021-10-20 23:14:32 +0200

    Add ProductEditModal, configured ProductItem and MainNavigation

--- src/ui/src/components/layout/MainNavigation.js
+++ src/ui/src/components/layout/MainNavigation.js
@@ -1,8 +1,16 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import ProductEditModal from '../ui/ProductEditModal';
+import { Button } from 'reactstrap';
 
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
+
+    const [isActive, setIsActive] = useState(false);
+
+    function toggle() { setIsActive(!isActive) };
+
     return (
         <header className={classes.header}>
             <div className={classes.logo}>Eat Well</div>
@@ -12,7 +20,8 @@ const MainNavigation = () => {
                         <Link to="/">All Products</Link>
                     </li>
                     <li>
-                        <Link to="/new-product">Add New Product</Link>
+                    <Link to="" onClick={toggle}>Add New Product</Link>
+                        { isActive && <ProductEditModal /> }
                     </li>
                 </ul>
             </nav>
--- src/ui/src/components/products/ProductItem.js
+++ src/ui/src/components/products/ProductItem.js
@@ -1,9 +1,14 @@
 import Card from '../ui/Card';
 import classes from './ProductItem.module.css';
+import React, { useState } from 'react';
+import ProductEditModal from '../ui/ProductEditModal';
 
 const ProductItem = (props) => {
+    const [isActive, setIsActive] = useState(false);
+
+    function toggle() { setIsActive(!isActive) };
     return (
-        <div className={classes.item}>
+        <li className={classes.item}>
             <Card>
                 <div className={classes.image}>
                     <img src={props.image} alt={props.title} />
@@ -13,11 +18,15 @@ const ProductItem = (props) => {
                     <p>{props.description}</p>
                 </div>
                 <div className={classes.actions}>
-                    <button>Edit</button>
+                    <button onClick={toggle}>Edit</button>
+                    { isActive && <ProductEditModal 
+                    productName = {props.title}
+                    productDescription = {props.description}
+                         /> }
                     <button>Delete</button>
                 </div>
             </Card>
-        </div>
+        </li>
     );
 };
 
