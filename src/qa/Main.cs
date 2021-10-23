5ab37b72167c2711d119def04828148e4f2b5960
M-Hasbi <kaynakhasbi1@gmail.com>
2021-10-23 14:45:51 +0200

    add: AllProductsClick() test AllProductsClick() test EditButton() test DeleteButton() test

--- src/qa/Main.cs
+++ src/qa/Main.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace EatWell.QA
-{
+{  // This Main.cs file is necessery, otherwise testexplorer is given an error about lacking of "Main" function.
     class MainClass
     {
         public static void Main(string[] args)
--- src/qa/UITests.cs
+++ src/qa/UITests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using System.Threading;
+using Assert = NUnit.Framework.Assert;
 
 namespace EatWell.QA
 {
@@ -10,11 +11,35 @@ namespace EatWell.QA
     public class UITests : TestBase
     {
         [Test]
-        public void AddNewProductButton()
+        public void AddNewProductClick()
         {
-            IWebElement addNewProductButton = driver.FindElement(By.XPath("//a[text()='Add New Product']"));
-            addNewProductButton.Click(); 
+            IWebElement addNewProductClick = driver.FindElement(By.XPath("//a[text()='Add New Product']"));
+            addNewProductClick.Click(); 
             Thread.Sleep(2000);
         }
+        [Test]
+        public void AllProductsClick()
+        {
+            IWebElement allProductsClick = driver.FindElement(By.XPath("//a[text()='All Products']"));
+            allProductsClick.Click();
+           // Assert.AreEqual();
+            Thread.Sleep(2000);
+        }
+        [Test]
+        public void EditButton()
+        {
+            IWebElement editButtonClick = driver.FindElement(By.XPath("//button[text()='Edit']"));
+            editButtonClick.Click();
+            Thread.Sleep(2000);
+        }
+        [Test]
+        public void DeleteButton()
+        {
+            IWebElement deleteButtonClick = driver.FindElement(By.XPath("//button[text()='Delete']"));
+            deleteButtonClick.Click();
+            Thread.Sleep(2000);
+        }
+
+
     }
 }
