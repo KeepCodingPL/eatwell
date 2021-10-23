7819099bae966bae9da12a5f9618c7a71b2ffca7
M-Hasbi <kaynakhasbi1@gmail.com>
2021-10-23 17:43:39 +0200

    add: assertions

--- src/qa/UITests.cs
+++ src/qa/UITests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using System.Configuration;
 using System.Threading;
 using Assert = NUnit.Framework.Assert;
 
@@ -12,31 +13,41 @@ namespace EatWell.QA
     {
         [Test]
         public void AddNewProductClick()
+        
         {
             IWebElement addNewProductClick = driver.FindElement(By.XPath("//a[text()='Add New Product']"));
-            addNewProductClick.Click(); 
+            addNewProductClick.Click();
+            string newProductUrl = "http://localhost:3000/new-product"; 
+            string currentUrl = driver.Url;
+            Assert.AreEqual(newProductUrl, currentUrl);
             Thread.Sleep(2000);
         }
         [Test]
         public void AllProductsClick()
         {
+            IWebElement addNewProductClick = driver.FindElement(By.XPath("//a[text()='Add New Product']"));
+            addNewProductClick.Click();
+            Thread.Sleep(2000);
             IWebElement allProductsClick = driver.FindElement(By.XPath("//a[text()='All Products']"));
             allProductsClick.Click();
-           // Assert.AreEqual();
+            string currentUrl = driver.Url;
+            Assert.AreEqual(ConfigurationManager.AppSettings["url"], currentUrl);
             Thread.Sleep(2000);
         }
         [Test]
-        public void EditButton()
+        public void EditButton1()
         {
-            IWebElement editButtonClick = driver.FindElement(By.XPath("//button[text()='Edit']"));
+            IWebElement editButtonClick = driver.FindElement(By.XPath("(//button[text()='Edit'])[1]"));
             editButtonClick.Click();
+            Assert.IsTrue(editButtonClick.Displayed);
             Thread.Sleep(2000);
         }
         [Test]
-        public void DeleteButton()
+        public void DeleteButton1()
         {
-            IWebElement deleteButtonClick = driver.FindElement(By.XPath("//button[text()='Delete']"));
+            IWebElement deleteButtonClick = driver.FindElement(By.XPath("(//button[text()='Delete'])[1]"));
             deleteButtonClick.Click();
+            Assert.IsTrue(deleteButtonClick.Displayed);
             Thread.Sleep(2000);
         }
 
