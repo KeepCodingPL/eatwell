30c932865772e7394e3d38a79ffbd373b9267f2f
M-Hasbi <kaynakhasbi1@gmail.com>
2021-10-23 14:18:17 +0200

    create: new Test for AddNewProductButton

--- src/qa/UITests.cs
+++ src/qa/UITests.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Threading;
 
 namespace EatWell.QA
 {
@@ -6,6 +9,12 @@ namespace EatWell.QA
     [TestClass]
     public class UITests : TestBase
     {
-
+        [Test]
+        public void AddNewProductButton()
+        {
+            IWebElement addNewProductButton = driver.FindElement(By.XPath("//a[text()='Add New Product']"));
+            addNewProductButton.Click(); 
+            Thread.Sleep(2000);
+        }
     }
 }
