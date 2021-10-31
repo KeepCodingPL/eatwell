5ac81dfad601249db2e0baadeaf9fd9ca8bfe08e
M-Hasbi <kaynakhasbi1@gmail.com>
2021-10-31 19:03:55 +0100

    create: AllProductsPage.cs

---
+++ src/qa/Pages/AllProductsPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EatWell.QA.Pages
+{
+    public class AllProductsPage : BasePage
+    {
+        string editButtonPath = "(//button[text()='Edit'])[1]";
+    }
+}
