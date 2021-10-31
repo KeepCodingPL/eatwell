1a4e8efa7fe052d4fd4a9cdac1e9d9634b6d9ba5
M-Hasbi <kaynakhasbi1@gmail.com>
2021-10-31 18:56:25 +0100

    refactore: DriverManager.cs requirements

--- src/qa/DriverManager.cs
+++ src/qa/DriverManager.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Configuration;
 
 namespace EatWell.QA
 {
-    class DriverManager
+    public class DriverManager
     {
+        public static IWebDriver driver = new ChromeDriver();
+        public string url = ConfigurationManager.AppSettings["url"];
+
+        /*
+         if (driver != null)
+            {
+                string browser = "Chrome";
+                switch (browser)
+                {
+                    case "Chrome":
+                        driver = new ChromeDriver();
+                        break;
+                    case "Firefox":
+                        driver = new FirefoxDriver();
+                        break;
+                    case "IE":
+                        driver = new InternetExplorerDriver();
+                        break;
+                    case "Edge":
+                        driver = new EdgeDriver();
+                        break;
+                }
+         */
     }
 }
