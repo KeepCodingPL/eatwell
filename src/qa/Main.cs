02c40c0e26a47fd4b002d6ee1a59610c3718987e
M-Hasbi <kaynakhasbi1@gmail.com>
2021-11-08 13:12:51 +0100

    refactor(qa) : remove unnecessary usings

--- src/qa/Main.cs
+++ src/qa/Main.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace EatWell.QA
+﻿namespace EatWell.QA
 {  // This Main.cs file is necessery, otherwise testexplorer is given an error about lacking of "Main" function.
     class MainClass
     {
