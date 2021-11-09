d49efd6aea145f5c53f161d70c4abcd0b82f405c
Enes-git <enes.3.ergun@gmail.com>
2021-11-09 22:11:42 +0100

    new-list-interface

--- src/ui/src/components/products/ProductItem.js
+++ src/ui/src/components/products/ProductItem.js
@@ -1,23 +1,32 @@
-import Card from '../ui/Card';
+import React, { useState } from 'react';
+
+import ProductEditModal from '../ui/ProductEditModal';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
+    const [isActive, setIsActive] = useState(false);
+
+    function toggle() {
+        setIsActive(!isActive);
+    }
+
     return (
-        <div className={classes.item}>
-            <Card>
-                <div className={classes.image}>
-                    <img src={props.image} alt={props.title} />
-                </div>
-                <div className={classes.content}>
-                    <h3>{props.title}</h3>
-                    <p>{props.description}</p>
-                </div>
-                <div className={classes.actions}>
-                    <button>Edit</button>
-                    <button>Delete</button>
-                </div>
-            </Card>
-        </div>
+        <li className={classes.item}>
+            <div className={classes.content}>
+                <h3>{props.title}</h3>
+                <p>{props.description}</p>
+            </div>
+            <div className={classes.actions}>
+                <button onClick={toggle}>Edit</button>
+                {isActive && (
+                    <ProductEditModal
+                        productName={props.title}
+                        productDescription={props.description}
+                    />
+                )}
+                <button>Delete</button>
+            </div>
+        </li>
     );
 };
 
