c07fff99c66b1a1ed4a48716e0af4938db3e6031
Şaban Durna <sabandurna@gmail.com>
2021-11-09 23:10:45 +0100

    refactor: make codes more simple

--- src/api/EatWell.Web/Services/ProductService.cs
+++ src/api/EatWell.Web/Services/ProductService.cs
@@ -12,33 +12,14 @@ namespace EatWell.API.Services
     {
         private readonly IProductRepository _productRepository;
 
-        public ProductService(IProductRepository productRepository)
-        {
-            _productRepository = productRepository;
-        }
+        public ProductService(IProductRepository productRepository) => _productRepository = productRepository;
+        
+        public void CreateProduct(ProductModel product) => _productRepository.CreateProduct(product);
 
-        public void CreateProduct(ProductModel product)
-        {
-
-            _productRepository.CreateProduct(product);
-
-        }
-
-        public void DeleteProduct(int id)
-        {
-            _productRepository.DeleteProduct(id);
-        }
-
-        public IEnumerable<ProductModel> GetProducts()
-        {
-            return _productRepository.GetProducts();
-        }
-
-        public void UpdateProduct(UpdateRequestDto product)
-        {
-
-            _productRepository.UpdateProduct(product);
-
-        }
+        public void DeleteProduct(int id) => _productRepository.DeleteProduct(id);
+        
+        public IEnumerable<ProductModel> GetProducts() => _productRepository.GetProducts();
+        
+        public void UpdateProduct(UpdateRequestDto product) => _productRepository.UpdateProduct(product);
     }
 }
