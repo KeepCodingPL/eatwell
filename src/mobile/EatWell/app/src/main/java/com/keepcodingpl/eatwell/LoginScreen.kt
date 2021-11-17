ff22340f715c8775023a80d130b240570d345453
Suleyman Oner <suleymanoner1999@gmail.com>
2021-11-17 19:28:44 +0100

    login activity added

---
+++ src/mobile/EatWell/app/src/main/java/com/keepcodingpl/eatwell/LoginScreen.kt
@@ -0,0 +1,14 @@
+package com.keepcodingpl.eatwell
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+
+        // https://eatwell-4dadc.firebaseapp.com/__/auth/handler
+
+    }
+}
