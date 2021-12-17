732de0babc1e51fa57edc242d84c13603c25758e
Ahmet Kuris <ahmetkuris2308@gmail.com>
2021-12-17 23:34:03 +0100

    feature(api): add mediatr

--- src/api/EatWell.Web/Startup.cs
+++ src/api/EatWell.Web/Startup.cs
@@ -2,18 +2,12 @@ using EatWell.API.Persistence;
 using EatWell.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
 
 namespace EatWell.Web
 {
@@ -42,6 +36,8 @@ namespace EatWell.Web
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "EatWell.Web", Version = "v1" });
             });
+
+            services.AddMediatR(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
